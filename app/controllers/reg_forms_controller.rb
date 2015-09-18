19dae9a21a57e87a7467969c3a5301949ecb313c
Anne Hawley Brett <annehawleybrett@gmail.com>
2015-09-18 17:51:38 -0400

    controllers, actions, and views for registration forms

---
+++ app/controllers/reg_forms_controller.rb
@@ -0,0 +1,25 @@
+class RegFormsController < ApplicationController
+  def new
+    @reg_form = RegForm.new
+  end
+
+  def show
+    @reg_form = RegForm.find(params[:id])
+  end
+
+  def create
+    @reg_form = RegForm.new(reg_form_params)
+    @reg_form.user_id = current_user.id
+
+    if @reg_form.save
+      render :new
+    else
+      render :new
+    end
+  end
+
+  private
+  def reg_form_params
+    params.require(:reg_form).permit(:name, :email, :phone, :user_id)
+  end
+end
