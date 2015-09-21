fecf551de798c8fd7738ae85f95e324ae6974e01
Anne Hawley Brett <annehawleybrett@gmail.com>
2015-09-21 17:03:40 -0400

    validations for user, regform, and addresses, error messages for user view

--- app/models/reg_form.rb
+++ app/models/reg_form.rb
@@ -3,4 +3,8 @@ class RegForm < ActiveRecord::Base
 
   has_one :address, :as => :addressable
   accepts_nested_attributes_for :address
+
+  validates :email, uniqueness: true
+  validates :name, presence: true
+  validates :phone, presence: true, uniqueness: true
 end
