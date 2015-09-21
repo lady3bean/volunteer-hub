36b927126f3edde80ba00a928dc01c7d518ab352
Anne Hawley Brett <annehawleybrett@gmail.com>
2015-09-21 17:05:08 -0400

    fixup! validations for user, regform, and addresses, error messages for user view

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
+
+  validates :line1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
 end
